37c4939ebc3936e8a73c4e3ccf21b575e8b3199e
Dirag Biswas <diragbiswas@gmail.com>
2020-05-14 00:20:30 +0530

    integrated firebase

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
+// Packages:
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as firebase from "firebase/app";
 import * as serviceWorker from './serviceWorker';
 
+
+// Imports:
+import './index.css';
+
+
+// Components:
+import App from './App';
+
+
+// Initialize Firebase:
+const firebaseConfig = {
+  apiKey: "AIzaSyDASoUlqdSLf1u7mxD1WXuZNi5fCZFhYgE",
+  authDomain: "darkboi-748ea.firebaseapp.com",
+  databaseURL: "https://darkboi-748ea.firebaseio.com",
+  projectId: "darkboi-748ea",
+  storageBucket: "darkboi-748ea.appspot.com",
+  messagingSenderId: "957406294719",
+  appId: "1:957406294719:web:55717bb752b2145ea8cc0a"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+// Render:
 ReactDOM.render(
   <React.StrictMode>
     <App />
